a401e518e2e4ff1ee53c8e6111e4df2a38c1a2a5
zainabmirza-eng <zainabmirzafatah99@gmail.com>
2024-03-15 20:43:48 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const array = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(array){
+    const newArray = array
+    return newArray.slice(0,2);
+}
+
+function returnLastTwoDrivers(array){
+    const newArray = array
+    return  newArray.slice(-2);
+}
+
+console.log(returnFirstTwoDrivers(array));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(a){
+    return function(b){
+        return b * a;
+    }
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(DriverArray, driverSelection){
+    return driverSelection(DriverArray);
+}
+    const FirstTwoDrivers = selectDifferentDrivers(DriverArray,driverSelection);
+    const LastTwoDrivers = selectDifferentDrivers(DriverArray, driverSelection);
+
+  
